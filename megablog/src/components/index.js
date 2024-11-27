49cb295824de362443abb59eae10504eb62cb3d3
shashank shukla <120403602+Shashank12604@users.noreply.github.com>
2024-11-27 18:00:40 +0530

    Add files via upload

---
+++ megablog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/container"
+import Logo from "./logo";
+import Button from "./button";
+import LogoutBtn from "./Header/logoutBtn"
+import RTE from "./RTE";
+import AuthLayout from "./AuthLayout"
+import PostCard from "./PostCard";
+import PostForm from "./postform/postform";
+import Input from "./input";
+import Select from "./select";
+import Signup from "./signup";
+import Login from "./login";
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Button,
+    LogoutBtn,
+    RTE,
+    AuthLayout,
+    PostCard,
+    PostForm,
+    Input,
+    Select,
+    Signup,
+    Login
+}
